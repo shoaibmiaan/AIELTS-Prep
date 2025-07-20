35c4362cb2d15fb0b1aa09b550192a1d053f229a
shoaibmiaan <shoaibmiaan@gmail.com>
2025-07-20 11:41:35 +0500

    "Added StudyStreak component, updated layout, and integrated study activity logging"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import { useAuth } from '@/context/AuthContext';
 import toast from 'react-hot-toast';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import HomeContent from '@/components/home/HomeContent';
+import HomeContent from '@/components/home/homeContent';
 import GuestContent from '@/components/home/GuestContent';
 import LoginModal from '@/components/home/LoginModal';
 
